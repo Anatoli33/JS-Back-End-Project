3866512621a49524794c51a06d99416848fe84d7
Anatoli33 <hadjiev1215@gmail.com>
2025-09-24 10:18:11 +0300

    Make handlebars works

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import express from "express"
-
+import handlebars from "express-handlebars";
 
 const app = express();
 const port = 3000;
 
+// Setup Handlebars
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
+// Setup static middleware
+app.use(express.static('src/public'));
+
 app.get('/', (req, res) => {
-res.status(200);
-res.send('Welcome to Express.js!');
+res.render('home', {layout: false});
 })
 app.listen(port, () => console.log(`Express running
 on port: ${port}...`));

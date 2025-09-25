f28b11d2bb16df0365fcf78433d85b6c4691c273
Anatoli33 <hadjiev1215@gmail.com>
2025-09-25 17:52:08 +0300

    Add 404 page

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,8 @@ const routes =  Router();
 
 routes.use(homeController);
 routes.use('/movies', movieController);
+routes.use('*splat', (req, res) =>{
+    res.render('404');
+});
 
 export default routes;

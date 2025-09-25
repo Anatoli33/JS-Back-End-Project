cd4fa3df7e65137f63c6118854bc2b8135a8be9b
Anatoli33 <hadjiev1215@gmail.com>
2025-09-25 10:45:35 +0300

    Show movies dynamic

--- src/module/Movie.js
+++ src/module/Movie.js
@@ -1,11 +1,11 @@
-const movies =  [
+const movies = [
   {
     title: "Inception",
     category: "Movie",
     genre: "Sci-Fi",
     director: "Christopher Nolan",
     year: 2010,
-    imageUrl: "https://m.media-amazon.com/images/I/51oDk9i8l0L._AC_.jpg",
+    imageUrl: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
     rating: 8.8,
     description: "A skilled thief enters the dreams of others to steal secrets, but faces his biggest challenge when asked to plant an idea instead.",
     casts: []
@@ -16,7 +16,7 @@ const movies =  [
     genre: "Crime",
     director: "Francis Ford Coppola",
     year: 1972,
-    imageUrl: "https://m.media-amazon.com/images/I/71xZ7U1EJ1L._AC_SY679_.jpg",
+    imageUrl: "https://image.tmdb.org/t/p/w500/d4KNaTrltq6bpkFS01pYtyXa09m.jpg",
     rating: 9.2,
     description: "The aging patriarch of an organized crime dynasty transfers control of his empire to his reluctant son.",
     casts: []
@@ -27,7 +27,7 @@ const movies =  [
     genre: "Adventure",
     director: "Christopher Nolan",
     year: 2014,
-    imageUrl: "https://m.media-amazon.com/images/I/91kFYg4fX3L._AC_SY679_.jpg",
+    imageUrl: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
     rating: 8.6,
     description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
     casts: []

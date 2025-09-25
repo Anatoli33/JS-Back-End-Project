36801ae3178e98dd884ae587a8bbc46680ddc26b
Anatoli33 <hadjiev1215@gmail.com>
2025-09-25 17:59:04 +0300

    Add details links for each movie

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,6 +4,7 @@ import movieService from "../services/movieService.js";
 const homeController = Router();
 
 homeController.get('/', (req, res) => {
+    
     const movies = movieService.getAll();
     res.render('home', {movies});
 });
--- src/modules/Movie.js
+++ src/modules/Movie.js
@@ -1,8 +1,8 @@
 import {v4 as uuid} from 'uuid';
 
 const movies = [
-
   {
+    _id: "f47ac10b-58cc-4372-a567-0e02b2c3d479",
     title: "Home Alone",
     category: "Movie",
     genre: "Comedy",
@@ -10,10 +10,10 @@ const movies = [
     year: 1990,
     imageUrl: "https://image.tmdb.org/t/p/w500/uuitWHpJwxD1wruFl2nZHIb4UGN.jpg",
     rating: 7.7,
-    description: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.",
-    casts: []
+    description: "An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation."
   },
   {
+    _id: "550e8400-e29b-41d4-a716-446655440000",
     title: "The Godfather",
     category: "Movie",
     genre: "Crime",
@@ -21,10 +21,10 @@ const movies = [
     year: 1972,
     imageUrl: "https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
     rating: 9.2,
-    description: "The aging patriarch of an organized crime dynasty transfers control of his empire to his reluctant son.",
-    casts: []
+    description: "The aging patriarch of an organized crime dynasty transfers control of his empire to his reluctant son."
   },
   {
+    _id: "123e4567-e89b-12d3-a456-426614174000",
     title: "Interstellar",
     category: "Movie",
     genre: "Adventure",
@@ -32,10 +32,9 @@ const movies = [
     year: 2014,
     imageUrl: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
     rating: 8.6,
-    description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
-    casts: []
+    description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival."
   }
-]
+];
 
 export default class Movie {
   constructor(data) {

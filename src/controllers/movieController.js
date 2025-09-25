7ad50ae93b2644cff6f544f0cad23090dfbae86b
Anatoli33 <hadjiev1215@gmail.com>
2025-09-25 11:04:41 +0300

    Show Create movie page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const movieController = Router();
 
 movieController.get('/movie/create', (req, res) => {
-    res.send('Movie create');
+    res.render('create');
 });
 
 export default movieController

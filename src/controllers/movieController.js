0edcae2d40224b752edd60efd6e1b5a2da0852b2
Anatoli33 <hadjiev1215@gmail.com>
2025-09-26 21:32:59 +0300

    Show static search page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -21,5 +21,8 @@ movieController.get("/:movieId/details", (req, res) =>{
     
     res.render('details', {movie});
 });
+movieController.get('/search', (req, res) =>{
+    res.render('search');
+})
 
 export default movieController;

e2c21c7722a2409da470f9317adba8d47523f429
Anatoli33 <hadjiev1215@gmail.com>
2025-09-27 22:18:31 +0300

    Allow prototypes in hbs

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,7 +3,7 @@ import Movie from "../modules/movie.js";
 export default{
     
     async getAll(filter){
-    const result = await Movie.find(filter).lean();
+    const result = await Movie.find(filter);
 
     // const resultObj = result.map(movie => movie.toObject());
 

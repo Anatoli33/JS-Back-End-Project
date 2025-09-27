3288d934f2a80661ad9f3b300d4a581b924025ba
Anatoli33 <hadjiev1215@gmail.com>
2025-09-27 21:56:52 +0300

    Sync movies in db with home screen

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,7 +3,7 @@ import Movie from "../modules/movie.js";
 export default{
     
     getAll(filter){
-    return Movie.find(filter);
+    return Movie.find();
 }, 
     getOne(movieId){
         return Movie.findOne({_id: movieId});

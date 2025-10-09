f111ab59fa0f791d27d3fd32328dabfb22dcfbb1
Anatoli33 <hadjiev1215@gmail.com>
2025-10-09 10:37:28 +0300

    Edit form

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -78,6 +78,8 @@ movieController.get('/:movieId/delete', isAuth, async (req, res) => {
 movieController.get('/:movieId/edit',  async (req, res) => {
    const movieId = req.params.movieId;
 
-   res.render('movies/edit');
+   const movie = await movieService.getOne(movieId);
+
+   res.render('movies/edit', { movie });
 });
 export default movieController;

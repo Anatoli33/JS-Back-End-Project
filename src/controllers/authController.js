4ebd1f5d30e32ebab2cd495971fd8866fa671e30
Anatoli33 <hadjiev1215@gmail.com>
2025-10-13 12:47:46 +0300

    Dynamic error message.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,13 +7,19 @@ const authController = Router();
 authController.get('/register',  isGuest, (req, res) =>{
     res.render('./auth/register');
 });
-authController.post('/register', async (req, res) =>{
+authController.post('/register', async (req, res) => {
     const userData = req.body;
 
-    await userService.register(userData);
-    
-    res.redirect('/auth/login');
+    try {
+        await userService.register(userData);
+        res.redirect('/auth/login');
+    } catch (err) {
+       const errorMessage = Object.values(err.errors).at(0).message;
+
+        res.status(400).render('auth/register', { error: errorMessage, user: userData })
+    }
 });
+
 authController.get('/login',  isGuest, (req, res) =>{
     res.render('./auth/login');
 });

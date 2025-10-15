e4a6d23477b3a53e83e7cfc3266944a61f543a7d
Anatoli33 <hadjiev1215@gmail.com>
2025-10-15 11:45:23 +0300

    Add cast error handling

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,19 +1,23 @@
 import { Router } from "express";
 import castService from "../services/castService.js";
-
+import { getErrorMessage } from './../utils/errorUtils.js';
 
 const castController = Router();
 
-castController.get('/create', (req, res) =>{
+castController.get('/create', (req, res) => {
     res.render('casts/create');
 });
-castController.post('/create', async(req, res) =>{
+
+castController.post('/create', async (req, res) => {
     const castData = req.body;
 
-    await castService.create(castData);
-
-    res.redirect('/');
-
-})
+    try {
+        await castService.create(castData);
+        res.redirect('/');
+    } catch (error) {
+        const errorMessage = getErrorMessage(error);
+        res.status(400).render('casts/create', { error: errorMessage, cast: castData });
+    }
+});
 
 export default castController;
